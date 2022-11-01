fc47ee2333b94c042edfd98188e7fc5eba737aea
James Martinez <james-martinez@users.noreply.github.com>
2022-11-01 10:54:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,16 +6,11 @@ variable "registry1_password" {
   type = string
 }
 
-variable "reduce_flux_resources" {
-  type = bool
-  default = true
-}
-
 # output "istio_gw_ip" {
 #   value = module.big_bang.external_load_balancer.ip
 # }
 module "big_bang" {
-  source = "git::https://repo1.dso.mil/platform-one/big-bang/terraform-modules/big-bang-terraform-launcher.git?ref=no-lb-check"
+  source = "git::https://repo1.dso.mil/platform-one/big-bang/terraform-modules/big-bang-terraform-launcher.git?ref=master"
 
   big_bang_manifest_file = "bigbang/start.yaml"
   registry_credentials = [{
@@ -23,5 +18,4 @@ module "big_bang" {
     username = var.registry1_username
     password = var.registry1_password
   }]
-  reduce_flux_resources = var.reduce_flux_resources
 }
